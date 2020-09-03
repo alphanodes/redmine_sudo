6417f8a218e5aa97338de55f4636565aaad70373
Claudia Meindl <c.meindl@alphanodes.com>
2020-09-03 19:13:14 +0200

    update language files ja, ru, zh, pt-br

--- test/unit/i18n_test.rb
+++ test/unit/i18n_test.rb
@@ -18,13 +18,13 @@ class I18nTest < RedmineSudo::TestCase
 
   def test_locales_validness
     lang_files_count = Dir[Rails.root.join('plugins/redmine_sudo/config/locales/*.yml')].size
-    assert_equal lang_files_count, 5
+    assert_equal lang_files_count, 9
     valid_languages.each do |lang|
       assert set_language_if_valid(lang)
       case lang.to_s
       when 'en'
         assert_equal 'Become Administrator', l(:label_become_admin)
-      when 'de', 'es', 'fr', 'it'
+      when 'pt-BR', 'de', 'es', 'fr', 'it', 'ja', 'ru', 'zh'
         assert_not l(:label_become_admin) == 'Become Administrator', lang
       end
     end
