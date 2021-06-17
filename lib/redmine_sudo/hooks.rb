244a5fc02b0320118a34b5085dca974f0b8416c6
Alexander Meindl <alex@alphanodes.com>
2021-06-17 18:50:40 +0200

    drop application directory

--- lib/redmine_sudo/hooks.rb
+++ lib/redmine_sudo/hooks.rb
@@ -2,7 +2,7 @@
 
 module RedmineSudo
   class RedmineSudoHookListener < Redmine::Hook::ViewListener
-    render_on :view_layouts_base_body_bottom, partial: 'sudo_body_bottom'
+    render_on :view_layouts_base_body_bottom, partial: 'sudo/body_bottom'
     render_on :view_my_account_contextual, partial: 'my/sudo_contextual'
   end
 end
