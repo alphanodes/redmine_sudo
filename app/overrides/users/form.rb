b0750c8dd677aeba56fa47e3cfeb2b54867075cb
Alexander Meindl <alex@alphanodes.com>
2021-12-02 16:15:54 +0100

    Fix Namespaces for autoloading

--- app/overrides/users/form.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new virtual_path: 'users/_form',
-                     name: 'users-replace-admin-with-sudo',
-                     replace: 'erb[loud]:contains("f.check_box :admin")',
-                     original: 'e60a4fa50dfadff1543026068000af0103515325',
-                     partial: 'users/sudo_checkbox'
---
+++ app/overrides/users_index.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module UsersIndex
+  Deface::Override.new virtual_path: 'users/index',
+                       name: 'users-list-replace-admin-header',
+                       replace: 'erb[loud]:contains("sort_header_tag(\'admin\'")',
+                       original: 'dc128c3cc172a8abe16a92d97e8fd20a2aa40427',
+                       partial: 'users/sudo_list_header'
+  Deface::Override.new virtual_path: 'users/index',
+                       name: 'users-list-replace-admin-column',
+                       replace_contents: 'td.tick',
+                       original: 'edc31bce1cac515de516f73c53f92a61c942301f',
+                       partial: 'users/sudo_list_column'
+end
