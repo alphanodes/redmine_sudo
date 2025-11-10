e054124e9149466d986eca2bdc9a1aaf394dab99
Alexander Meindl <a.meindl@alphanodes.com>
2025-11-10 16:54:43 +0100

    Use request.fullpath for toggle back_url to preserve current page with query parameters, enables SAML sudo_mode support

--- lib/redmine_sudo/helpers.rb
+++ lib/redmine_sudo/helpers.rb
@@ -5,7 +5,7 @@ module RedmineSudo
     def toggle_link_info
       admin = User.current.admin?
 
-      { url: toggle_sudo_path(back_url: url_for(request.params)),
+      { url: toggle_sudo_path(back_url: request.fullpath),
         title: admin ? l(:label_drop_admin_permission) : l(:label_become_admin),
         css: admin ? 'sudo-admin' : 'sudo-user',
         icon: admin ? 'fas_angle-double-down' : 'fas_angle-double-up' }
